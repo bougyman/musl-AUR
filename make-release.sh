018b5620d4ed11011700db25a5f93f328d793460
TJ Vanderpoel <tj@rubyists.com>
2014-07-13 11:31:30 -0500

    new release, conflicting with -mainline, this package will follow 1.0.x

--- make-release.sh
+++ make-release.sh
@@ -4,7 +4,7 @@ curdir=$PWD
 cd $curdir
 makepkg -g >> PKGBUILD
 vim PKGBUILD
-makepkg --source
+mkaurball
 if [ -v AUR_USER ];then
   burp -u $AUR_USER -p $(<~/.aurpass) -c system -C ~/.burpcookies ${pkgname}-${pkgver}-${pkgrel}.src.tar.gz
 else
